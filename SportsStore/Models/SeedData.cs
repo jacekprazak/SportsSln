645c3e194ddc6666bfd6bc13dd059e7e0757a199
Jacek Prazak <jacekprazak1987@gmail.com>
2025-12-03 10:58:18 +0100

    SeedData file added to project

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kajak",
+                        Description = "Łódka przeznaczona dla jednej osoby",
+                        Category = "Sporty wodne",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kamizelka ratunkowa",
+                        Description = "Chroni i dodaje uroku",
+                        Category = "Sporty wodne",
+                        Price = 48.95M
+                    },
+                    new Product
+                    {
+                        Name = "Piłka",
+                        Description = "Zatwierdzone przez FIFA rozmiar i waga",
+                        Category = "Piłka nożna",
+                        Price = 19.50M
+                    },
+                    new Product
+                    {
+                        Name = "Flagi narożne",
+                        Description = "Nadadzą twojemu boisku profesjonalny wygląd",
+                        Category = "Piłka nożna",
+                        Price = 34.95M
+                    },
+                    new Product
+                    {
+                        Name = "Stadion",
+                        Description = "Składany stadion na 35 000 osób",
+                        Category = "Piłka nożna",
+                        Price = 79500
+                    },
+                    new Product
+                    {
+                        Name = "Czapka",
+                        Description = "Zwiększa efektywność mózgu o 75%",
+                        Category = "Szachy",
+                        Price = 16
+                    },
+                    new Product
+                    {
+                        Name = "Niestabilne krzesło",
+                        Description = "Zmniejsza szanse przeciwnika",
+                        Category = "Szachy",
+                        Price = 29.95M
+                    },
+                    new Product
+                    {
+                        Name = "Ludzka szachownica",
+                        Description = "Przyjemna gra dla całej rodziny",
+                        Category = "Szachy",
+                        Price = 75
+                    },
+                    new Product
+                    {
+                        Name = "Błyszczący król",
+                        Description = "Figurka pokryta złotem i wysadzana diamentami",
+                        Category = "Szachy",
+                        Price = 1200
+                    }
+
+                );
+            }
+        }
+    }
+}
