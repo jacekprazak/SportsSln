800e7a0ddf06ba18a1719d49ed2499dfe0b7b00b
Jacek Prazak <jacekprazak1987@gmail.com>
2025-12-04 17:42:47 +0100

    new test added (Can_Send_Pagination_View_Model) to ProductControllerTest

--- SportsStore.Tests/ProductControllerTest.cs
+++ SportsStore.Tests/ProductControllerTest.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -58,6 +59,33 @@ namespace SportsStore.Tests
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" }
+            }).AsQueryable<Product>);
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            //Act
+            ProductListViewModel result =
+                controller.Index(2).ViewData.Model as ProductListViewModel;
+
+            //Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
         }
     }
 }
